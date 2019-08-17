0288712a45f270ed06e352511c3114e622627871
alexk08 <50315517+alexk08@users.noreply.github.com>
2019-08-17 14:35:06 +0400

    Добавляет js на меню и модальное окно

---
+++ source/js/script.js
@@ -0,0 +1,16 @@
+// var link = document.querySelector(".js-order-button");
+// var popup = document.querySelector(".modal-add-cart");
+
+// link.addEventListener("click",function (evt) {
+//     evt.preventDefault();
+//     popup.classList.add("js-modal-show");
+// });
+
+// window.addEventListener("keydown", function (evt) {
+//     if (evt.keyCode === 27) {
+//       evt.preventDefault();
+//       if (popup.classList.contains("js-modal-show")) {
+//         popup.classList.remove("js-modal-show");
+//       }
+//     }
+//   });
